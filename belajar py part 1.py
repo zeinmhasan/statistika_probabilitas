cb3f344d17ac22ed0dd5afee75e2930710a144f0
Zein Hasan <hasro71@gmail.com>
2024-09-07 23:03:35 +0700

    first commit

---
+++ belajar py part 1.py
@@ -0,0 +1,74 @@
+#tes print printan
+print ("hai sayang")
+print ("love u so much")
+print ("Hello " + "im ultaramen")
+print ("How do you make a hot dog stand?")
+print ("You take away its chair!")
+
+#pervariabelan duniawi
+todays_date = "7 sep 2024"
+product = 20*5
+remainder = 1398%11
+
+#sedikit kalkulasi python
+january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+annual_rainfall = january_to_june_rainfall
+
+july_rainfall = 1.05
+annual_rainfall += july_rainfall
+
+august_rainfall = 4.91
+annual_rainfall += august_rainfall
+
+september_rainfall = 5.16
+october_rainfall = 7.20
+november_rainfall = 5.06
+december_rainfall = 4.06
+september_to_december_rainfall = september_rainfall + october_rainfall + november_rainfall + december_rainfall
+annual_rainfall += september_to_december_rainfall
+print (annual_rainfall)
+
+cucumbers = 10
+price_per_cucumber = 3.25
+total_cost = cucumbers * price_per_cucumber
+print (total_cost)
+
+#tes komen
+city_name = "St. Potatosburg"
+# halo geys semuanyah
+city_pop = 340000
+
+#belajar data float
+cucumbers = 100
+num_people = 6
+whole_cucumbers_per_person = cucumbers/num_people
+print (whole_cucumbers_per_person)
+float_cucumbers_per_person = float(cucumbers)/num_people
+print (float_cucumbers_per_person)
+
+#multi string
+haiku = """The old pond,
+A frog jumps in:
+Plop!"""
+
+# Hi! I'm Maria and I live in script.py.
+# I'm an expert Python coder.
+# I'm 21 years old and I plan to program cool stuff forever.
+age_is_12 = False
+name_is_maria = True
+
+#pengabungan variabel yg berbeda jenis
+float_1 = 0.25
+float_2 = 40.0
+product = float_1 * float_2
+big_string = "The product was " + str(product)
+print (big_string)
+
+#rankuman semuanya yg di atas
+skill_completed = "Python Syntax"
+exercises_completed = 13
+points_per_exercise = 5
+point_total = 100
+point_total += exercises_completed * points_per_exercise
+#The amount of points for each exercise may change, because points don't exist yet
+print ("I got " + str(point_total) + " points!")
---
+++ belajar py part 2.py
@@ -0,0 +1,42 @@
+#metode pada string
+parrot = "Norwegian Blue"
+print (len(parrot))
+
+parrot = "Norwegian Blue"
+print (parrot.lower())
+
+parrot = "norwegian blue"
+print (parrot.upper())
+
+#mengubah data float menjadi data string
+pi = 3.14
+print (str(pi))
+
+#sistem per printan
+ministry = "The Ministry of Silly Walks"
+print (len(ministry))
+print (ministry.upper())
+
+the_machine_goes = "Ping!"
+print (the_machine_goes)
+
+print ("Spam " + "and " + "eggs")
+
+print ("The value of pi is around " + str(3.14))
+
+string_1 = "Camelot"
+string_2 = "place"
+print ("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
+
+name = "Alex"
+quest = "Teaching Python"
+color = "Blue"
+
+print ("Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color))
+
+#kesimpulan!
+
+my_string = "Lia cantik"
+print (len(my_string))
+print (my_string.upper())
